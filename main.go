c143df3793a4d34aa8cbe7912657fa0bee91591c
Đặng Đức <ducdang513@gmail.com>
2022-09-10 15:45:14 +0700

    Connect database

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
+
 import "github.com/gofiber/fiber/v2"
+import "gorm.io/gorm"
+import "gorm.io/driver/mysql"
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:DucDV@31@/mysql"), &gorm.Config{})
+
+	if err != nil {
+		panic(any("Could not connect to the database"))
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
